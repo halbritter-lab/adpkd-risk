211a71c411d00c2124209bea0c89b3180bc16b38
Bernt Popp <bernt.popp@gmail.com>
2024-09-08 15:34:23 +0200

    chore: Update Vite dependency to version 5.4.3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',  // Adjust this to match your deployment subdirectory
   plugins: [vue()],
 })
