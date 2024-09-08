98eec68596b3dc3b2b1ea5ddda04d734db06cebd
Bernt Popp <bernt.popp@gmail.com>
2024-09-08 13:45:47 +0200

    refactor: Update browser tab title dynamically from config

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// src/config.js
+export const APP_CONFIG = {
+    title: 'ADPKD Risk Calculator',
+    // You can add more app configurations here in the future
+  };
+  
--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
 
 import { createApp } from 'vue';
 import App from './App.vue';
-
-// Import the Vuetify plugin
-import vuetify from './plugins/vuetify'; 
-
+import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
+import { APP_CONFIG } from './config';  // Import the configuration
 
 loadFonts();
 
-createApp(App)
-  .use(vuetify) // Use the Vuetify plugin
-  .mount('#app');
+const app = createApp(App)
+  .use(vuetify);
+
+// Dynamically set the browser tab title from config
+document.title = APP_CONFIG.title;
+
+app.mount('#app');
