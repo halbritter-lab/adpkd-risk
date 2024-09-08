79137e367532adff515c9a97762e3fda083481ab
Bernt Popp <bernt.popp@gmail.com>
2024-09-08 15:47:41 +0200

    chore: Update deployment subdirectory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/adpkd-risk/" : "/",
+  base: "/adpkd-risk/",
   plugins: [vue()],
 })
